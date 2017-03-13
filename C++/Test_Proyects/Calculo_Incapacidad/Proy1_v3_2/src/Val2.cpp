8b3408ee543583245e5e677a8b01c44d1ebf989d
bactilago <bactilago@gmail.com>
2017-03-13 10:49:44 -0600

    Adding Proy1v3_2

---
+++ C++/Test_Proyects/Calculo_Incapacidad/Proy1_v3_2/src/Val2.cpp
@@ -0,0 +1,14 @@
+/*
+ * Val2.cpp
+ *
+ *  Created on: Mar 11, 2017
+ *      Author: Human Waste
+ */
+
+#include "Val2.h"
+
+Val2::Val2() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ C++/Test_Proyects/Calculo_Incapacidad/Proy1_v3_2/src/Val2.h
@@ -0,0 +1,16 @@
+/*
+ * Val2.h
+ *
+ *  Created on: Mar 11, 2017
+ *      Author: Human Waste
+ */
+
+#ifndef VAL2_H_
+#define VAL2_H_
+
+class Val2 {
+public:
+	Val2();
+};
+
+#endif /* VAL2_H_ */
