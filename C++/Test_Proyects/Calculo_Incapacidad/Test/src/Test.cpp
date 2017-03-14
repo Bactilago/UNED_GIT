2be9d5efc5c3a7c4149c10f1f5a1f1bbdabd3696
bactilago <bactilago@gmail.com>
2017-03-13 18:15:10 -0600

    update

---
+++ C++/Test_Proyects/Calculo_Incapacidad/Test/src/Test.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : Test.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "Cons.h"
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	string shit;
+	shit = string Cons::asignName(shit);
+	return 0;
+}
---
+++ C++/Test_Proyects/Calculo_Incapacidad/Test_Constr/src/ConsEmpl.h
@@ -0,0 +1,21 @@
+/*
+ * ConsEmpl.h
+ *
+ *  Created on: Mar 13, 2017
+ *      Author: Human Waste
+ */
+
+#ifndef CONSEMPL_H_
+#define CONSEMPL_H_
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+class ConsEmpl {
+public:
+	ConsEmpl();
+	string getName();
+};
+
+#endif /* CONSEMPL_H_ */
